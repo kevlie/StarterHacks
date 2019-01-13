a2437158edff65bdbad2993a568816bf0eb6e49a
kevlie <kevinlie10@gmail.com>
2019-01-13 05:06:34 -0500

    Added SQLAlchemy

--- Learnify/__init__.py
+++ Learnify/__init__.py
@@ -1,11 +1,16 @@
 import os
 
 from flask import Flask, render_template, url_for
-
+from flask_sqlalchemy import SQLAlchemy
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+
+    # Setting up database for playlists.
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+    p_db = SQLAlchemy(app)
+
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
