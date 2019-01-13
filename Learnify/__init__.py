34ed9cb478bf1beee5baf4270fedfaca9ac63edf
Kelvin Fan <kelvinfan001@gmail.com>
2019-01-13 00:27:26 -0500

    started to implement scraper

--- Learnify/__init__.py
+++ Learnify/__init__.py
@@ -30,6 +30,10 @@ def create_app(test_config=None):
     from . import auth
     app.register_blueprint(auth.bp)
 
+    from . import index
+    app.register_blueprint(index.bp)
+    app.add_url_rule('/', endpoint='index')
+
     # a simple page that says hello
     @app.route('/hello')
     def hello():
